27c5d366c4c6aab0ab8d81da801171d8220b10ab
Administrator <1246771571@qq.com>
2019-02-26 18:13:57 +0800

    视屏json的搭建完成

--- app/src/main/java/com/example/administrator/smartcity/activities/VideoActivity.java
+++ app/src/main/java/com/example/administrator/smartcity/activities/VideoActivity.java
@@ -1,27 +1,61 @@
 package com.example.administrator.smartcity.activities;
 
-
-import android.net.Uri;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.administrator.smartcity.R;
 
-import io.vov.vitamio.Vitamio;
+import butterknife.BindView;
+import butterknife.OnClick;
 
-public class VideoActivity extends AppCompatActivity {
-
+public class VideoActivity extends BaseActivity implements View.OnClickListener {
     private VideoView videoView;
-    private String url2 = "http://flashmedia.eastday.com/newdate/news/2016-11/shznews1125-19.mp4";
+    private Button btnStart;
+    private Button btnPause;
+    private Button btnBack;
 
+    private String url2 = "http://flashmedia.eastday.com/newdate/news/2016-11/shznews1125-19.mp4";
+    private String url1 = "https://www.bilibili.com/video/av18781504/";
+    @Override
+    public Object getLayout() {
+        return R.layout.activity_video;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        videoView = (VideoView) findViewById(R.id.video_view);
+    public void onCreate() {
+        setNoTitleBarAndFullScreen();
+        setImmersionStatusBar();
+        videoView = findViewById(R.id.vv);
+        btnStart = findViewById(R.id.btn_pause);
+        btnPause = findViewById(R.id.btn_start);
+        btnBack = findViewById(R.id.btn_back);
+        videoView.setVideoPath(url2);
+        btnBack.setOnClickListener(this);// 点击事件使用 @onClick更快速
+        btnStart.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start:
+                if (!videoView.isPlaying()){
+                    videoView.start();
+                }
+                Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_pause:
+                Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_back:
+                Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/administrator/smartcity/base/impl/VideoPage.java
+++ app/src/main/java/com/example/administrator/smartcity/base/impl/VideoPage.java
@@ -3,6 +3,8 @@ package com.example.administrator.smartcity.base.impl;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -11,30 +13,27 @@ import android.widget.TextView;
 import com.example.administrator.smartcity.R;
 import com.example.administrator.smartcity.activities.VideoActivity;
 import com.example.administrator.smartcity.base.BasePage;
+import com.example.administrator.smartcity.utils.net.HttpUtils;
 
 /**
  * Create by SunnyDay on 2019/02/21
+ *     视屏界面开发
  */
 public class VideoPage extends BasePage {
     public VideoPage(Activity activity) {
         super(activity);
     }
+
     @Override
     public void initData() {
         tvTitle.setText("快看");
-
-        View view = View.inflate(activity,R.layout.video,null);
-
-        Button btn = view.findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity.startActivity(new Intent(activity,VideoActivity.class));
-            }
-        });
-
-
+        View view = View.inflate(activity, R.layout.video, null);
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_video);
+        LinearLayoutManager manager = new LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(manager);
+        // 请求网络
+        //HttpUtils.OkHttp();
         flLayout.addView(view);
-        // 类似抖音短视屏使用
+
     }
 }
