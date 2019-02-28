bb23bd11ef283bcc534bff9c2663a4c40878c998
Administrator <1246771571@qq.com>
2019-02-28 12:11:41 +0800

    SettingView 左侧文字的完善

--- app/src/main/java/com/example/administrator/smartcity/base/impl/SettingPage.java
+++ app/src/main/java/com/example/administrator/smartcity/base/impl/SettingPage.java
@@ -1,12 +1,14 @@
 package com.example.administrator.smartcity.base.impl;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.administrator.smartcity.R;
 import com.example.administrator.smartcity.base.BasePage;
+import com.example.administrator.smartcity.views.SettingView;
 
 /**
  * Create by SunnyDay on 2019/02/21
@@ -15,15 +17,16 @@ public class SettingPage extends BasePage {
     public SettingPage(Activity activity) {
         super(activity);
     }
+
     @Override
     public void initData() {
         tvTitle.setText("设置");
         btnMenu.setVisibility(View.GONE);
-        View view = View.inflate(activity,R.layout.page_setting,null);
-
-
+        // 设置界面的view  （内部使用自定义）
+        View view = View.inflate(activity, R.layout.page_setting, null);
+        SettingView accountManager = view.findViewById(R.id.account_manager);
+        accountManager.setLeftText("账号管理");
         flLayout.addView(view);
-
 
     }
 }
--- app/src/main/java/com/example/administrator/smartcity/views/SettingView.java
+++ app/src/main/java/com/example/administrator/smartcity/views/SettingView.java
@@ -1,30 +1,108 @@
 package com.example.administrator.smartcity.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.administrator.smartcity.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Create by SunnyDay on 2019/02/27
  * <p>
- * 设置界面的通用view    自定义view
+ * 设置界面的通用view    （自定义实现）
+ * <p>
+ * 功能：快速搭建设置界面
+ * 1 左侧文字的代码设置实现       定义xml属性实现
+ * 2 右侧的圆头像代码设置实现     自定义xml属性实现
+ * 3 右侧文本的代码设置实现       自定义xml属性实现
+ * 4 头像的显示隐藏   （代码实现，xml自定义实现）
+ * 5 文本的显示隐藏    （代码实现，xml自定义实现）
  */
 public class SettingView extends LinearLayout {
+    // 命名空间  所有自定义控件都可以使用这个特定字符串，AS自动帮你找。
+    private static final String NAME_SPACE = "http://schemas.android.com/apk/res-auto";
+
+    private TextView leftText;
+    private CircleImageView headPortrait;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     public SettingView(Context context) {
         this(context, null);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     public SettingView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     public SettingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        View view  = LayoutInflater.from(context).inflate(R.layout.view_setting_page,null);
+        View view = LayoutInflater
+                .from(context)
+                .inflate(R.layout.view_setting_page, null);
+
+        leftText = view.findViewById(R.id.text);
+        headPortrait = view.findViewById(R.id.img);
+
+        initSelfAttrs(attrs);
+
+        this.addView(view);
+    }
+
+    /**
+     * 自定义属性初始化工作
+     *
+     * @param attrs 属性集合
+     */
+    private void initSelfAttrs(AttributeSet attrs) {
+        // 1 左侧text的xml设置
+        String leftText = attrs.getAttributeValue(NAME_SPACE, "leftText");
+        setLeftText(leftText);
+        // 2 圆头像 xml实现
+        Integer resourceId = attrs.getAttributeIntValue(NAME_SPACE,"rightPic",0);
+        boolean showRightPic = attrs.getAttributeBooleanValue(NAME_SPACE, "showRightPic", false);
+        if (showRightPic){
+            headPortrait.setVisibility(VISIBLE);
+            setHeadPortrait(resourceId);
+        }
+    }
+
+    /**
+     * 设置左侧文字的内容
+     */
+    public void setLeftText(String text) {
+        leftText.setText(text);
+    }
+
+    /**
+     * 设置头像
+     *
+     * @param imgType 图片的类型
+     */
+    public void setHeadPortrait(Object imgType) {
+        if (imgType instanceof Drawable) {
+            headPortrait.setImageDrawable((Drawable) imgType);
+        } else if (imgType instanceof Integer) {
+            headPortrait.setImageResource((Integer) imgType);
+        } else if (imgType instanceof Bitmap) {
+            headPortrait.setImageBitmap((Bitmap) imgType);
+        } else if (imgType instanceof Uri) {
+            headPortrait.setImageURI((Uri) imgType);
+        } else {
+            throw new IllegalArgumentException("imgType just can be type:Drawable or recourceId or Bitmap or Uri");
+        }
 
     }
 
