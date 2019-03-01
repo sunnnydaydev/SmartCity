6add1fd8038e7b7a1aecfc6933aa3e7659bf580a
Administrator <1246771571@qq.com>
2019-03-01 16:43:47 +0800

    app finish

--- app/src/main/java/com/example/administrator/smartcity/base/impl/SettingPage.java
+++ app/src/main/java/com/example/administrator/smartcity/base/impl/SettingPage.java
@@ -35,7 +35,6 @@ public class SettingPage extends BasePage implements View.OnClickListener {
         view.findViewById(R.id.remove_cache).setOnClickListener(this);
         view.findViewById(R.id.amost_setting).setOnClickListener(this);
 
-
         flLayout.addView(view);
 
     }
@@ -51,7 +50,7 @@ public class SettingPage extends BasePage implements View.OnClickListener {
                 break;
             case R.id.about:
                 // todo 用户协议html页面
-                Toast.makeText(activity, "明天开发", Toast.LENGTH_SHORT).show();
+                Toast.makeText(activity, "h5小哥正在努力赶来", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.remove_cache:
                 Toast.makeText(activity, "待续!!!", Toast.LENGTH_SHORT).show();
